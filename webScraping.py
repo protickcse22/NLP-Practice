41a4afc2b6e2a760d054cc71c9bd2416860ec0ff
protick <kprotick@softbdltd.com>
2019-07-01 11:56:06 +0600

    added more news paper

--- webScraping.py
+++ webScraping.py
@@ -3,15 +3,15 @@ from bs4 import BeautifulSoup
 import csv
 from urllib.request import urlopen
 
-res = urlopen('https://www.priyo.com/')
-sope = BeautifulSoup(res, 'lxml')
+res = urlopen('http://www.bbarta24.net/')
+sope = BeautifulSoup(res, 'html.parser')
 var = sope.find_all('a')
 print(type(var))
 print(len(var))
 for link in sope.find_all('a'):
     print(link.text)
 
-with open('priyo.csv', 'w', encoding='utf-8') as csv_file:
+with open('bbarta24.csv', 'w', encoding='utf-8') as csv_file:
     csv_file.write("Link" + "\n")
     for link in sope.find_all('a'):
         csv_file.write(''.join(link.text.split(',')) + "\n")
